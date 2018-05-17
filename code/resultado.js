73ed8686d85b193f4022cc5bb80032ff878b75be
Flavio <flavio98.fernandes@gmail.com>
2018-05-17 20:51:36 +0100

    Janela Resultados  (Trocar do fim do jogo para aqui)

---
+++ code/resultado.js
@@ -0,0 +1,27 @@
+"use strict";
+
+(function()
+{
+	window.addEventListener("load", main);
+}());
+
+function main()
+{
+		var queryString = decodeURIComponent(window.location.search);
+		queryString = queryString.substring(1);
+		var queries = queryString.split("&");
+
+		audio1.volume = 0.3;
+
+		if(queries[1] == 0){
+			audio1.volume =0;
+		}
+
+		var menuBtn = document.getElementById("menuBtn");
+
+
+		menuBtn.addEventListener("click", function(){
+			document.location.href = "menu.html" + "?" + queries[0] + "&" + queries[1];
+		});
+
+}
